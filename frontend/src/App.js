81af60d475717fd80b1cc40197266bbabde33823
fatihyazicioglu <fy134388@gmail.com>
2022-12-06 15:09:29 +0100

    signUp screen created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 import Badge from "react-bootstrap/Badge";
 import Nav from "react-bootstrap/Nav";
 import { useContext } from "react";
 import { Store } from "./Store";
 import { Link } from "react-router-dom";
-import CartScreen from './screens/CartScreen';
-import SigninScreen from './screens/SigninScreen';
+import CartScreen from "./screens/CartScreen";
+import SigninScreen from "./screens/SigninScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
-
+import SignupScreen from "./screens/SignupScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart, userInfo } = state;
 
   const signoutHandler = () => {
-    ctxDispatch({ type: 'USER_SIGNOUT' });
-    localStorage.removeItem('userInfo');
+    ctxDispatch({ type: "USER_SIGNOUT" });
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("shippingAddress");
   };
   return (
     <BrowserRouter>
       <div className='d-flex flex-column site-container'>
-      <ToastContainer position="bottom-center" limit={1} />
+        <ToastContainer position='bottom-center' limit={1} />
 
         <header>
           <Navbar bg='dark' variant='dark'>
@@ -46,24 +47,24 @@ function App() {
                   )}
                 </Link>
                 {userInfo ? (
-                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
-                    <LinkContainer to="/profile">
+                  <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
+                    <LinkContainer to='/profile'>
                       <NavDropdown.Item>User Profile</NavDropdown.Item>
                     </LinkContainer>
-                    <LinkContainer to="/orderhistory">
+                    <LinkContainer to='/orderhistory'>
                       <NavDropdown.Item>Order History</NavDropdown.Item>
                     </LinkContainer>
                     <NavDropdown.Divider />
                     <Link
-                      className="dropdown-item"
-                      to="#signout"
+                      className='dropdown-item'
+                      to='#signout'
                       onClick={signoutHandler}
                     >
                       Sign Out
                     </Link>
                   </NavDropdown>
                 ) : (
-                  <Link className="nav-link" to="/signin">
+                  <Link className='nav-link' to='/signin'>
                     Sign In
                   </Link>
                 )}
@@ -75,13 +76,14 @@ function App() {
           <Container>
             <Routes>
               <Route path='/product/:slug' element={<ProductScreen />} />
-              <Route path="/signin" element={<SigninScreen />} />
-              <Route path="/cart" element={<CartScreen />} />
+              <Route path='/signin' element={<SigninScreen />} />
+              <Route path='/cart' element={<CartScreen />} />
               <Route path='/' element={<HomeScreen />} />
               <Route
-                path="/shipping"
+                path='/shipping'
                 element={<ShippingAddressScreen />}
               ></Route>
+              <Route path='/signup' element={<SignupScreen />} />
             </Routes>
           </Container>
         </main>
