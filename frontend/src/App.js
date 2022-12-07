e5aa3ec8c0ad26d17b0382ce7620732416b118aa
fatihyazicioglu <fy134388@gmail.com>
2022-12-07 12:05:59 +0100

    PlaceorderScreen created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import SigninScreen from "./screens/SigninScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignupScreen";
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -87,6 +88,7 @@ function App() {
                 element={<ShippingAddressScreen />}
               ></Route>
               <Route path='/signup' element={<SignupScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
             </Routes>
           </Container>
         </main>
